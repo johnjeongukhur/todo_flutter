6676908224bde5fa630e1b7a05d448fa5d0bb49e
johnhur <johnjeongukhur@gmail.com>
2023-10-27 13:39:15 +0900

    add: Edit 되고나면 TodoView로 돌아가게끔 추가

--- lib/ViewModel/todoedit_viewmodel.dart
+++ lib/ViewModel/todoedit_viewmodel.dart
@@ -3,6 +3,7 @@ import 'package:first_flutter_project/View/tododetail_view.dart';
 import 'package:first_flutter_project/View/todoedit_view.dart';
 import 'package:get/get.dart';
 import '../Repositories/todo_repository.dart';
+import '../View/todo_view.dart';
 
 class TodoEditViewModel extends GetxController {
   var isLoading = false.obs;
@@ -13,7 +14,7 @@ class TodoEditViewModel extends GetxController {
     try {
       isLoading(true);
       var data = await _todoRepository.putTodo(id, title, description, priority);
-      // Get.offAll(TodoDetailView(id: id));
+      Get.offAll(TodoView());
     } catch (error) {
       print('Failed from viewmodel');
     } finally {
