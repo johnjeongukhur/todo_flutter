396867c1b92c93589a455c1a49cc3a6a8910fc37
johnhur <johnjeongukhur@gmail.com>
2023-12-05 11:04:49 +0900

    add: android local url

--- lib/API/api_client.dart
+++ lib/API/api_client.dart
@@ -9,7 +9,8 @@ class ApiClient {
 
   static BaseOptions _getOptions() {
     return BaseOptions(
-      baseUrl: 'http://127.0.0.1:8000',
+      baseUrl: 'http://127.0.0.1:8000', // iOS
+      // baseUrl: 'http://10.0.2.2:8000', // Android
       connectTimeout: Duration(seconds: 5), // 연결 대기 시간
       receiveTimeout: Duration(seconds: 3), // 응답 대기 시간
     );
